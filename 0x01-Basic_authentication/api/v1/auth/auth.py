a380a8c4b8550b15a3501840005fd105e3430486
abdelhak <chenafa.abdelhak@gmail.com>
2024-11-13 22:44:59 +0000

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
+"""Auth module"""
 from typing import List, TypeVar
 from flask import request
-"""Auth module"""
 
 
 class Auth:
     """ Auth class
     """
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> None:
         """ Determines if authentication is required. """
         return False
 
-
-    def authorization_header(self, request=None) -> str:
+    def authorization_header(self, request=None) -> None:
         """ Returns the value of the Authorization header. """
         return None
 
-
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> None:
         """ Returns the current user (to be implemented later). """
         return None
-
