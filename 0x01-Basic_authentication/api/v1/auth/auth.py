2eef060d98c65e6bbb01de977a4359fdfec870b3
abdelhak <chenafa.abdelhak@gmail.com>
2024-11-13 22:55:55 +0000

    Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,9 +8,21 @@ class Auth:
     """ Auth class
     """
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> None:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Determines if authentication is required. """
-        return False
+        if path is None:
+            return True
+
+        if not excluded_path:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> None:
         """ Returns the value of the Authorization header. """
