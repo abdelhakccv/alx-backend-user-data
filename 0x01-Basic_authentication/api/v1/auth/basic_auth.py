a153f859ba632d55ce332a776ac2a64972344652
abdelhak <chenafa.abdelhak@gmail.com>
2024-11-15 18:33:31 +0000

    base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -19,3 +19,15 @@ class BasicAuth(Auth):
         if authorization_header[:6] != "Basic ":
             return None
         return authorization_header[6:]
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception
+            return None
